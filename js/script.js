0005756af7c962616b492b91ee423785dee6d407
HilsonF <hilson_francis@Hilsons-MacBook-Pro.local>
2015-04-04 12:16:51 -0400

    commit 1
    fist commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+(function ($) {
+  
+  $.jvmobilemenu({
+    mainContent: $('.page'),
+    theMenu: $('.mobile-nav'),
+    slideSpeed: 0.3,
+    menuWidth: 240,
+    position: 'left',
+    menuPadding: '20px 20px 60px'
+  });
+
+  $('.mobile-nav .mobile-menu').removeAttr('overflow-y');
+  
+})(jQuery);
